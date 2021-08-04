bba45aec538707b22ae9f4f7540d4dedff59e6c2
Peter <pebueku@gmail.com>
2021-08-04 22:26:23 +0100

    Added a foreign key 'auth.User 'to website\models.py

--- website/models.py
+++ website/models.py
@@ -5,6 +5,7 @@ class Website(models.Model):
     url = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
