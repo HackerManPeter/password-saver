98d43ed49d2aa5d6a9c91a3ddf86348209f95938
Peter <pebueku@gmail.com>
2021-08-04 21:52:16 +0100

    Added a website model to website\models.py

--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Website(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        pass
