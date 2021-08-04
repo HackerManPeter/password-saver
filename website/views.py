d454dbc73bf1cb6cff67da6b45f2b7c53f549474
Peter <pebueku@gmail.com>
2021-08-04 21:56:20 +0100

    Added WebsiteListView to website\views.py

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Website
+
+class WebsiteListView(generic.ListView):
+    model = Website
+    template_name = 'website\index.html'
+    context_object_name = 'website'
