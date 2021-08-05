2d854a18a59ad250ed47f7d945789438b948e087
Peter <pebueku@gmail.com>
2021-08-05 16:51:46 +0100

    Added UpdateView and DetailView to views.py

--- website/views.py
+++ website/views.py
@@ -7,3 +7,12 @@ class WebsiteListView(generic.ListView):
     model = Website
     template_name = 'website\index.html'
     context_object_name = 'website'
+
+class WebsiteDetailView(generic.DetailView):
+    model = Website
+    template_name = 'website\detail.html'
+
+class WebsiteUpdateView(generic.UpdateView):
+    model = Website
+    template_name = "website\edit.html"
+    fields = {'website_name', 'website_url', 'username', 'password'}
