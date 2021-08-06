5e654d8289ecd420b44393fa0ea5f528d85f77bb
Peter <pebueku@gmail.com>
2021-08-06 12:23:43 +0100

    Added urls for 'CreateView' and 'DeleteView'

--- website/urls.py
+++ website/urls.py
@@ -8,5 +8,7 @@ app_name = 'website'
 urlpatterns = [
     path('', views.WebsiteListView.as_view(), name="home"),
     path('site/<int:pk>/', views.WebsiteDetailView.as_view(), name="detail"),
-    path('site/<int:pk>/edit', views.WebsiteUpdateView.as_view(), name='edit')
+    path('site/<int:pk>/edit', views.WebsiteUpdateView.as_view(), name='edit'),
+    path('site/<int:pk>/delete', views.WebsiteDeleteView.as_view(), name='delete'),
+    path('site/create', views.WebsiteCreateView.as_view(), name='create'),
 ]
